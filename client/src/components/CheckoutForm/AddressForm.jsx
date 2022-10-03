795d918b9d644fce6b8f0cf6d0ca9690203214e8
mherring11 <m.herring11@yahoo.com>
2022-10-02 19:33:30 -0500

    finished checkout

--- client/src/components/CheckoutForm/AddressForm.jsx
+++ client/src/components/CheckoutForm/AddressForm.jsx
@@ -8,7 +8,7 @@ import { commerce } from '../../lib/commerce';
 import FormInput from './CustomTextField';
 
 
-const AddressForm = ({ checkoutToken} ) => {
+const AddressForm = ({ checkoutToken}, next ) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -58,7 +58,7 @@ const AddressForm = ({ checkoutToken} ) => {
     <>
         <Typography variant='h6' gutterBottom>Shipping Address</Typography>
         <FormProvider {...methods}>
-            <form onSubmit=''>
+            <form onSubmit={methods.handleSubmit((data) => next({ ... data,shippingCountry,shippingSubdivision, shippingOption }))}>
                 <Grid container spacing={3}>
                     <FormInput name='firstName' label='First name' />
                     <FormInput name='lastName' label='Last name' />
