ed538ef0b7bb16b19cc83afee7a1bebe0150e29d
Strider <dave.barnes23@gmail.com>
2022-10-06 18:04:29 -0500

    added the path module to the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { generateToken } = require('./utils/auth');
@@ -28,16 +29,16 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
-    
-    db.once('open', () => {
-      app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-      })
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
-    };
+  })
+};
     
     // Call the async function to start the server
     startApolloServer(typeDefs, resolvers);
