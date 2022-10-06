944170369824be04e733c028cf2531ac15aa43ee
Strider <dave.barnes23@gmail.com>
2022-10-06 18:30:58 -0500

    changed the title and footer to reflect the 360 sports

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-          <p className="mb-3 mb-md-0">Click-n-Ship 2022 {" "}</p>
+          <p className="mb-3 mb-md-0">360 Sports 2022 {" "}</p>
           </div>
         </div>
       </footer>
